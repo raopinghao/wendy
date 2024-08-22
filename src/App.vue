035edfc97ab5bf97b691274325a3e3721da19fc9
饶平号 <781226250@qq.com>
2024-08-22 14:43:51 +0800

    啊啊啊啊

--- src/App.vue
+++ src/App.vue
@@ -3,14 +3,7 @@ import HelloWorld from "./components/HelloWorld.vue";
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />22222
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
+  <div>wendy的demo</div>
   <HelloWorld msg="Vite + Vue" />
 </template>
 
